3e0c4099ae21d17d0002621af8e2e7b0f4abda37
datnguyen <dattannguyentan04@gmail.com>
2024-06-11 02:40:09 +0700

    Upload lab

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductlistComponent } from './components/productlist/productlist.component';
+import { ProductdetailComponent } from './components/productdetail/productdetail.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { CartComponent } from './components/cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'products', component: ProductlistComponent},
+    {path: 'products/:id', component: ProductdetailComponent},
+    {path: 'products/:id/cart', component: CartComponent},
+    {path: '**', component: NotfoundComponent}
+];
+
